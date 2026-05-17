3740c5612c4ec637e11af7d6c4175b29b5e162f1
Ciara <ciarafomunung@gmail.com>
2026-05-17 18:58:27 +0100

    feat: recipe controller

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ const recipeRoutes = require('./src/routes/recipe.routes');
 
 const app = express();
 
+app.use(express.json());
 app.use('/api/recipes', recipeRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  const status = err.status || 500;
+  res.status(status).json({
+    message: err.message || 'Internal server error',
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const {
+  getAllRecipes,
+  getRecipeById,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe,
+} = require('../controllers/recipe.controller');
 
 const router = express.Router();
+
+router.get('/', getAllRecipes);
+router.get('/:id', getRecipeById);
+router.post('/', createRecipe);
+router.patch('/:id', updateRecipe);
+router.delete('/:id', deleteRecipe);
 
 module.exports = router;
